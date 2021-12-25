4a27658d314d024a402317bc6dcb33537bf0bfc0
Shogo-Matsumoto0412 <66903388+Shogo-Matsumoto0412@users.noreply.github.com>
2021-12-26 03:18:09 +0900

    update

--- README.md
+++ README.md
@@ -3778,3 +3778,173 @@ const Detail = () => {
 
 export default Detail;
 ```
+
+## 全体検索機能
+
+src/commons/ListTable.jsx
+
+```
+import React, { memo, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { createLike, deleteLike } from "../api/like";
+
+const ListTable = memo((props) => {
+  const { dataList, handleDelete, currentUser, handleGetList } = props;
+
+  // ここから追加
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [value, setValue] = useState("");
+  // ここまで追加
+
+  // 追加
+  const handleSearch = (e) => {
+    setValue(e.target.value);
+  };
+
+  // 追加
+  useEffect(() => {
+    const newItems = dataList.filter((item) => {
+      return (
+        item.title.toLowerCase().indexOf(value) !== -1 ||
+        item.content.toLowerCase().indexOf(value) !== -1
+      );
+    });
+    setFilteredItems(newItems);
+  }, [value, dataList]);
+
+  const handleCreateLike = async (item) => {
+    try {
+      const res = await createLike(item.id);
+      console.log(res.data);
+      handleGetList();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleDeleteLike = async (item) => {
+    try {
+      const res = await deleteLike(item.id);
+      console.log(res.data);
+      handleGetList();
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <>
+      // ここから追加
+      <form action="">
+        <label htmlFor="search">検索</label>
+        <input type="text" id="search" onChange={handleSearch} />
+      </form>
+      // ここまで追加
+      <table>
+        <thead>
+          <tr>
+            <th>メールアドレス</th>
+            <th>タイトル</th>
+            <th>内容</th>
+            <th>いいね</th>
+            <th></th>
+            <th></th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          // ここから修正
+          {!filteredItems ? (
+            <>
+              {dataList.map((item) => (
+                <tr key={item.id}>
+                  <td>
+                    <Link to={`/users/${item.userId}`}>{item.email}</Link>
+                  </td>
+                  <td>{item.title}</td>
+                  <td>{item.content}</td>
+                  <td>
+                    {item.likes.find(
+                      (like) => like.userId === currentUser.id
+                    ) ? (
+                      <p onClick={() => handleDeleteLike(item)}>
+                        ♡{item.likes.length}
+                      </p>
+                    ) : (
+                      <p onClick={() => handleCreateLike(item)}>
+                        ♡{item.likes.length}
+                      </p>
+                    )}
+                  </td>
+                  {currentUser.id === item.userId ? (
+                    <td>
+                      <Link to={`/edit/${item.id}`}>更新</Link>
+                    </td>
+                  ) : (
+                    <td></td>
+                  )}
+                  <td>
+                    <Link to={`/post/${item.id}`}>詳細へ</Link>
+                  </td>
+                  {currentUser.id === item.userId ? (
+                    <td>
+                      <button onClick={() => handleDelete(item)}>削除</button>
+                    </td>
+                  ) : (
+                    <td></td>
+                  )}
+                </tr>
+              ))}
+            </>
+          ) : (
+            <>
+              {filteredItems.map((item) => (
+                <tr key={item.id}>
+                  <td>
+                    <Link to={`/users/${item.userId}`}>{item.email}</Link>
+                  </td>
+                  <td>{item.title}</td>
+                  <td>{item.content}</td>
+                  <td>
+                    {item.likes.find(
+                      (like) => like.userId === currentUser.id
+                    ) ? (
+                      <p onClick={() => handleDeleteLike(item)}>
+                        ♡{item.likes.length}
+                      </p>
+                    ) : (
+                      <p onClick={() => handleCreateLike(item)}>
+                        ♡{item.likes.length}
+                      </p>
+                    )}
+                  </td>
+                  {currentUser.id === item.userId ? (
+                    <td>
+                      <Link to={`/edit/${item.id}`}>更新</Link>
+                    </td>
+                  ) : (
+                    <td></td>
+                  )}
+                  <td>
+                    <Link to={`/post/${item.id}`}>詳細へ</Link>
+                  </td>
+                  {currentUser.id === item.userId ? (
+                    <td>
+                      <button onClick={() => handleDelete(item)}>削除</button>
+                    </td>
+                  ) : (
+                    <td></td>
+                  )}
+                </tr>
+              ))}
+            </>
+          )}
+          // ここまで修正
+        </tbody>
+      </table>
+    </>
+  );
+});
+
+export default ListTable;
+```
--- frontend/src/commons/ListTable.jsx
+++ frontend/src/commons/ListTable.jsx
@@ -1,9 +1,25 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { createLike, deleteLike } from "../api/like";
 
 const ListTable = memo((props) => {
   const { dataList, handleDelete, currentUser, handleGetList } = props;
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [value, setValue] = useState("");
+
+  const handleSearch = (e) => {
+    setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    const newItems = dataList.filter((item) => {
+      return (
+        item.title.toLowerCase().indexOf(value) !== -1 ||
+        item.content.toLowerCase().indexOf(value) !== -1
+      );
+    });
+    setFilteredItems(newItems);
+  }, [value, dataList]);
 
   const handleCreateLike = async (item) => {
     try {
@@ -26,58 +42,112 @@ const ListTable = memo((props) => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>メールアドレス</th>
-          <th>タイトル</th>
-          <th>内容</th>
-          <th>いいね</th>
-          <th></th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {dataList.map((item) => (
-          <tr key={item.id}>
-            <td>
-              <Link to={`/users/${item.userId}`}>{item.email}</Link>
-            </td>
-            <td>{item.title}</td>
-            <td>{item.content}</td>
-            <td>
-              {item.likes.find((like) => like.userId === currentUser.id) ? (
-                <p onClick={() => handleDeleteLike(item)}>
-                  ♡{item.likes.length}
-                </p>
-              ) : (
-                <p onClick={() => handleCreateLike(item)}>
-                  ♡{item.likes.length}
-                </p>
-              )}
-            </td>
-            {currentUser.id === item.userId ? (
-              <td>
-                <Link to={`/edit/${item.id}`}>更新</Link>
-              </td>
-            ) : (
-              <td></td>
-            )}
-            <td>
-              <Link to={`/post/${item.id}`}>詳細へ</Link>
-            </td>
-            {currentUser.id === item.userId ? (
-              <td>
-                <button onClick={() => handleDelete(item)}>削除</button>
-              </td>
-            ) : (
-              <td></td>
-            )}
+    <>
+      <form action="">
+        <label htmlFor="search">検索</label>
+        <input type="text" id="search" onChange={handleSearch} />
+      </form>
+      <table>
+        <thead>
+          <tr>
+            <th>メールアドレス</th>
+            <th>タイトル</th>
+            <th>内容</th>
+            <th>いいね</th>
+            <th></th>
+            <th></th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {!filteredItems ? (
+            <>
+              {dataList.map((item) => (
+                <tr key={item.id}>
+                  <td>
+                    <Link to={`/users/${item.userId}`}>{item.email}</Link>
+                  </td>
+                  <td>{item.title}</td>
+                  <td>{item.content}</td>
+                  <td>
+                    {item.likes.find(
+                      (like) => like.userId === currentUser.id
+                    ) ? (
+                      <p onClick={() => handleDeleteLike(item)}>
+                        ♡{item.likes.length}
+                      </p>
+                    ) : (
+                      <p onClick={() => handleCreateLike(item)}>
+                        ♡{item.likes.length}
+                      </p>
+                    )}
+                  </td>
+                  {currentUser.id === item.userId ? (
+                    <td>
+                      <Link to={`/edit/${item.id}`}>更新</Link>
+                    </td>
+                  ) : (
+                    <td></td>
+                  )}
+                  <td>
+                    <Link to={`/post/${item.id}`}>詳細へ</Link>
+                  </td>
+                  {currentUser.id === item.userId ? (
+                    <td>
+                      <button onClick={() => handleDelete(item)}>削除</button>
+                    </td>
+                  ) : (
+                    <td></td>
+                  )}
+                </tr>
+              ))}
+            </>
+          ) : (
+            <>
+              {filteredItems.map((item) => (
+                <tr key={item.id}>
+                  <td>
+                    <Link to={`/users/${item.userId}`}>{item.email}</Link>
+                  </td>
+                  <td>{item.title}</td>
+                  <td>{item.content}</td>
+                  <td>
+                    {item.likes.find(
+                      (like) => like.userId === currentUser.id
+                    ) ? (
+                      <p onClick={() => handleDeleteLike(item)}>
+                        ♡{item.likes.length}
+                      </p>
+                    ) : (
+                      <p onClick={() => handleCreateLike(item)}>
+                        ♡{item.likes.length}
+                      </p>
+                    )}
+                  </td>
+                  {currentUser.id === item.userId ? (
+                    <td>
+                      <Link to={`/edit/${item.id}`}>更新</Link>
+                    </td>
+                  ) : (
+                    <td></td>
+                  )}
+                  <td>
+                    <Link to={`/post/${item.id}`}>詳細へ</Link>
+                  </td>
+                  {currentUser.id === item.userId ? (
+                    <td>
+                      <button onClick={() => handleDelete(item)}>削除</button>
+                    </td>
+                  ) : (
+                    <td></td>
+                  )}
+                </tr>
+              ))}
+            </>
+          )}
+        </tbody>
+      </table>
+    </>
   );
 });
 
